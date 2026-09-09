f09fe409f0a9f759bce1cf4c2c25e89012ba2689
Amankumar014 <aman113114kumarsah@gmail.com>
2026-09-10 01:12:08 +0530

    added the new api

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,7 +2,7 @@
 // Automatically uses Azure backend URL in production, localhost in development
 
 const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://self-portfolio-backend-e8crhzfza4cbg2bq.southeastasia-01.azurewebsites.net'
+  ? 'https://aman-website-backend-dsaqhtd7fsgcdae7.indiasouthcentral-01.azurewebsites.net'
   : 'http://localhost:5000';
 
 export default API_BASE_URL;
